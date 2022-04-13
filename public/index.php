9c0ad491c45d7beb64bb4b2c601ace3fd7fb7b8d
Janic De Luca <janic.deluca@payrexx.com>
2022-04-13 09:03:29 +0200

    Add Example for mysqli Connection

--- public/index.php
+++ public/index.php
@@ -1,2 +1,13 @@
 <?php
-phpinfo();
+
+$databaseConn = new mysqli('mariadb', $_ENV['MARIADB_USER'], $_ENV['MARIADB_PASSWORD'], $_ENV['MARIADB_DATABASE']);
+
+if($databaseConn->connect_error) {
+    die("Connection failed: " . $databaseConn->connect_error);
+}
+echo "Connected!";
+
+$mysqliResult = $databaseConn->query("SELECT * FROM user");
+while ($row = $mysqliResult->fetch_object()) {
+    echo $row->id;
+}
