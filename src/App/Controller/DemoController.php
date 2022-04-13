ec78457ed38bebba9ade3e1f2af990bd9dab8a82
Janic De Luca <janic.deluca@payrexx.com>
2022-04-13 14:00:43 +0200

    Implement features from Learnpath

--- src/App/Controller/DemoController.php
+++ src/App/Controller/DemoController.php
@@ -2,10 +2,25 @@
 
 namespace Controller;
 
+use Service\DatabaseService;
+
 class DemoController
 {
 	public function route()
 	{
 		require __DIR__ . '/../html/demo.html';
 	}
+
+    public function getUser($id) {
+        $stmt = (new DatabaseService)->getConnection()->prepare("SELECT * FROM user WHERE id LIKE :id");
+        $stmt->execute(['id' => $id]);
+
+        var_dump($stmt->fetchAll());
+    }
+
+    public function getUsers() {
+        $stmt = (new DatabaseService)->getConnection()->query("SELECT * FROM user");
+
+        var_dump($stmt->fetchAll());
+    }
 }
