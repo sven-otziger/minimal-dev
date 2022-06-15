2826e56a166780d94554c09be1728e1b669ffded
Sven Otziger <sven.otziger@payrexx.com>
2022-06-15 17:13:43 +0200

    refactor to const

--- src/App/Routes/routes.php
+++ src/App/Routes/routes.php
@@ -1,70 +1,72 @@
 <?php
 // routing with symfony
 // ::class		returns fully qualified classname
+const CONTROLLER = '_controller';
+
 return [
 	[
 		'path' => '/blog/{slug}',
-		'controller' => ['_controller' => \Controller\BlogController::class],
+		'controller' => [CONTROLLER => \Controller\BlogController::class],
 		'name' => 'blog_show'
 	],
 	[
 		'path' => '/demo/index',
-		'controller' => ['_controller' => \Controller\DemoController::class],
+		'controller' => [CONTROLLER => \Controller\DemoController::class],
 		'name' => 'route'
 	],
 	[
 		'path' => '/retro/{slug}',
-		'controller' => ['_controller' => \Controller\RetroController::class],
+		'controller' => [CONTROLLER => \Controller\RetroController::class],
 		'name' => 'serialization'
 	],
 	[
 		'path' => '/readUsers',
-		'controller' => ['_controller' => \Controller\UserController::class],
+		'controller' => [CONTROLLER => \Controller\UserController::class],
 		'name' => 'readUsers'
 	],
 	[
 		'path' => '/readUser/{id}',
-		'controller' => ['_controller' => \Controller\UserController::class],
+		'controller' => [CONTROLLER => \Controller\UserController::class],
 		'name' => 'readUser'
 	],
 	[
 		'path' => '/createUser',
-		'controller' => ['_controller' => \Controller\UserController::class],
+		'controller' => [CONTROLLER => \Controller\UserController::class],
 		'name' => 'createUser'
 	],
 	[
 		'path' => '/create-user-form',
-		'controller' => ['_controller' => \Controller\UserController::class],
+		'controller' => [CONTROLLER => \Controller\UserController::class],
 		'name' => 'createUserForm'
 	],
 	[
 		'path' => '/updateUser/{id}/{username}/{password}',
-		'controller' => ['_controller' => \Controller\UserController::class],
+		'controller' => [CONTROLLER => \Controller\UserController::class],
 		'name' => 'updateUser'
 	],
 	[
 		'path' => '/deleteUser/{id}',
-		'controller' => ['_controller' => \Controller\UserController::class],
+		'controller' => [CONTROLLER => \Controller\UserController::class],
 		'name' => 'deleteUser'
 	],
 	[
 		'path' => '/readUserWhereUsernameLike/{search}',
-		'controller' => ['_controller' => \Controller\UserController::class],
+		'controller' => [CONTROLLER => \Controller\UserController::class],
 		'name' => 'readUserWhereUsernameLike'
 	],
 	[
 		'path' => '/display/{id}',
-		'controller' => ['_controller' => \Controller\UserController::class],
+		'controller' => [CONTROLLER => \Controller\UserController::class],
 		'name' => 'display'
 	],
 	[
 		'path' => '/orm',
-		'controller' => ['_controller' => \Controller\UserController::class],
+		'controller' => [CONTROLLER => \Controller\UserController::class],
 		'name' => 'orm'
 	],
 	[
 		'path' => '/home',
-		'controller' => ['_controller' => \Controller\WebsiteController::class],
+		'controller' => [CONTROLLER => \Controller\WebsiteController::class],
 		'name' => 'home'
 	]
 ];
