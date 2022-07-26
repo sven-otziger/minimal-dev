88370aeedbb1472011b9720313dc5eec9b216fbb
Sven Otziger <sven.otziger@payrexx.com>
2022-07-26 08:19:50 +0200

    update/create enum

---
+++ src/App/Enum/Attribute.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Enum;
+
+Enum Attribute
+{
+    case Username;
+    case Password;
+    case IsDeleted;
+}
---
+++ src/App/Enum/User.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Enum;
+
+Enum User: string
+{
+    case username = 'username';
+    case password = 'password';
+    case street = 'street';
+    case houseNumber = 'house_number';
+    case zip = 'zip_code';
+    case city = 'city';
+    case age = 'age';
+
+}
