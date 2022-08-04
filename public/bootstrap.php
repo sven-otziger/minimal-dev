f167c9ca2471fc2358da8175e0765f34cfd54053
Sven Otziger <sven.otziger@payrexx.com>
2022-08-04 15:19:22 +0200

    pre mapping commands

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -9,7 +9,9 @@ $isDevMode = true;
 $proxyDir = null;
 $cache = null;
 $useSimpleAnnotationReader = false;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . "../src"), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
+$config = Setup::createAnnotationMetadataConfiguration(array(dirname(__DIR__) . "/src"), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
+//$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . "\..\src"), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
+
 // or if you prefer yaml or XML
 // $config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
 // $config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
---
+++ src/App/Entity/Movie.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="movie")
+ */
+class Movie
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    private int $id;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $title;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $director;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private int $length;
+
+    /**
+     * Rating out of 10
+     *
+     * @ORM\Column(type="integer")
+     */
+    private int $rating;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDirector(): string
+    {
+        return $this->director;
+    }
+
+    /**
+     * @param string $director
+     */
+    public function setDirector(string $director): void
+    {
+        $this->director = $director;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLength(): int
+    {
+        return $this->length;
+    }
+
+    /**
+     * @param int $length
+     */
+    public function setLength(int $length): void
+    {
+        $this->length = $length;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRating(): int
+    {
+        return $this->rating;
+    }
+
+    /**
+     * @param int $rating
+     */
+    public function setRating(int $rating): void
+    {
+        $this->rating = $rating;
+    }
+
+}
